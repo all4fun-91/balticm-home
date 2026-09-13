bc15fde4f746a6e362ee3a8418f110e0e16e9919
all4fun-91 <all4fun91@gmail.com>
2026-09-13 14:10:27 +0200

    Keep upper footer line, remove only lower copyright line

--- azuriom-theme/balticm/views/home.blade.php
+++ azuriom-theme/balticm/views/home.blade.php
@@ -5,6 +5,7 @@
 
 @section('app')
 <style>
+    .bm-footer { border-top: 1px solid var(--bm-line) !important; }
     .bm-copy { border-top: 0 !important; }
     html, body { overflow-x: hidden !important; }
     .bm-community-strip {
