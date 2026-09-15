319a54369f6fbf6ef6a4554f5c35e3728d4c386e
all4fun-91 <all4fun91@gmail.com>
2026-09-15 03:10:38 +0200

    Hide navbar on password reset page

--- azuriom-theme/balticm/views/layouts/app.blade.php
+++ azuriom-theme/balticm/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     @if(request()->is('user/password/reset*'))
         <style>
             html,body{background:#020711 url('https://media.balticm.eu/media/site/1789346383673-4e25ebb7-52fc-45bb-8fcb-f9c96c46b873.png') center top/cover fixed no-repeat!important}
+            header{display:none!important}
         </style>
     @endif
     <main class="bm-page bm-wrap">
